45ad001897f11c507bbc0967cb91289cbfce29f1
周烜杰 <17858803852@163.com>
2019-07-04 15:42:56 +0800

    update submodule

--- note.submodule.js
+++ note.submodule.js
@@ -1,11 +1,15 @@
 // git公共仓库管理方案
 
 /*
-git submodule add<仓库名> //添加submodule
+git submodule add <仓库名> //添加submodule
 
 git submodule init //clone父项目后init submodule
 
 git submodule update //手动更新submodule代码
 
 git clone --recursive https://github.com/chaconinc/MainProject //clone项目时自动初始化并更新仓库中的每一个子模块。git pull 貌似没有
+
+git config -f .gitmodules submodule.<仓库名>.branch <远程分支名> //将submodule关联到特定分支
+
+git submodule update --remote <仓库名> //进入submodule然后抓取并更新
 */
