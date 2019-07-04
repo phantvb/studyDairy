5f5d7e8e6624ce2e6b162cb7e07a41158c3c030b
周烜杰 <17858803852@163.com>
2019-07-04 15:41:49 +0800

    add submodule

---
+++ note.submodule.js
@@ -0,0 +1,11 @@
+// git公共仓库管理方案
+
+/*
+git submodule add<仓库名> //添加submodule
+
+git submodule init //clone父项目后init submodule
+
+git submodule update //手动更新submodule代码
+
+git clone --recursive https://github.com/chaconinc/MainProject //clone项目时自动初始化并更新仓库中的每一个子模块。git pull 貌似没有
+*/
